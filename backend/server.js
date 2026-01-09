48f5ac95a007784d73b65f5ad78f14a4621473d0
Luke Zhuang <Lukezhuang@yahoo.com>
2026-01-08 17:20:33 -0800

    created a post method for practice, added status code along with get and post methods

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.send("you got 5 notes");
+  res.status(200).send("you got 5 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).send("your note has been created successfully");
 });
 
 app.listen(5001, () => {
