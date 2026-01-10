3e4198c82b1ac39492ee325c038cbdb17a3d10fb
Luke Zhuang <Lukezhuang@yahoo.com>
2026-01-09 17:25:56 -0800

    now i have get, post, put and delete endpoints implemented on server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
-  res.status(201).send("your note has been created successfully");
+  res.status(201).json({ message: "Note created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note deleted successfully!" });
 });
 
 app.listen(5001, () => {
