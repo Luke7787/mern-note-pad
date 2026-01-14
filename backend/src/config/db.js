52eaab5a34bdef3e2dc348b233a87477719e1e36
Luke Zhuang <Lukezhuang@yahoo.com>
2026-01-13 17:09:01 -0800

    created an .env value which holds mongodb connection string and port number, called its environment variable respectfully from .env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://lukezhuang_db_user:7KIvhiOC5m5gAQql@cluster0.jfmptcs.mongodb.net/notes_db?appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED SUCCESSFULLY!");
   } catch (error) {
     console.error("Error connecting to MONGODB", error);
