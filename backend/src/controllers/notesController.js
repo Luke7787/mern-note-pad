dc8214fa95311f99962832c9888db2ff5d4e5e4e
Luke Zhuang <Lukezhuang@yahoo.com>
2026-01-14 16:43:48 -0800

    implemented getAllNotes controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,13 @@
-export function getAllNotes(req, res) {
-  res.status(200).send("you just fetched the notes");
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 export function createNote(req, res) {
   res.status(201).json({ message: "Note created successfully!" });
