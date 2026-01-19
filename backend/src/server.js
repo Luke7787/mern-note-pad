c45789167836d8510fbc5ab957fbc848bab4e6cd
Luke Zhuang <Lukezhuang@yahoo.com>
2026-01-18 17:33:22 -0800

    changed PORT to 5005, implemented rateLimter middleware, set limit to 100 req per minute in upstash, called rateLimiter middleware in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5005;
 
 connectDB();
 
 // middleware
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
 // our simple custom middleware
 // app.use((req, res, next) => {
